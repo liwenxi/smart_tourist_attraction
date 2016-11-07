cd01944236d25cd49401609f2ae84c4829abd768
李文熙 <liwenxi@hahac.local>
2016-11-07 10:50:18 +0800

    Initial Commit

---
+++ smart_tourist_attraction/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  smart_tourist_attraction
+//
+//  Created by 李文熙 on 16/11/7.
+//  Copyright © 2016年 李文熙. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
